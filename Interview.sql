bf3eec444152cf75bc02142ca3f0546381c30da6
Kristopher Kivutha <kkibwana@gmail.com>
2022-07-17 13:58:10 +0300

    question 1 answer

--- Interview.sql
+++ Interview.sql
@@ -1,7 +1,20 @@
 -- Question 1:
--- Create a simple tables countries and regions.
--- Countries table includes columns country_id, country_name and region_id
--- which already exist.
+-- Create simple tables countries and regions.
+-- Countries table includes columns country_id, country_name and region_id which already exist.
+CREATE TABLE IF NOT EXISTS countries
+(
+    country_id character varying(50) NOT NULL,
+    country_name character varying(250) NOT NULL,
+    region_id integer,
+    PRIMARY KEY (country_id)
+);
+
+CREATE TABLE regions
+(
+    region_id integer NOT NULL,
+    region_name character varying(250) NOT NULL,
+    PRIMARY KEY (region_id)   
+);
 
 -- Question 2:
 -- Write a SQL statement insert rows from the country_new table to countries
